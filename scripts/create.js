272475ab6bf3a6273f4d493bc5247ea18d787102
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2020-06-06 19:35:33 -0300

    populate data uf/city in 'createpage'

--- scripts/create.js
+++ scripts/create.js
@@ -1,16 +1,43 @@
 
+function searchUf(){
+    const ufSelect = document.querySelector("select[name=uf]")
 
-function 
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then( res =>  res.json())
+        .then( states => {
+
+            for (const state of states){
+                ufSelect.innerHTML += ` <option value="${state.id}">${state.nome}</option> `
+            } 
+        } )
+}
+
+searchUf()
 
 
+function getCities(event){
+    
+    const citySelect = document.querySelector("select[name=city]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    
+    fetch(url)
+        .then( res =>  res.json())
+        .then( cities => {
+
+            for (const city of cities){
+                citySelect.innerHTML += ` <option value="${city.id}">${city.nome}</option> `
+            } 
+
+            citySelect.disabled = false
+        } )
+}
+
+document.querySelector("select[name=uf").addEventListener("change", getCities)
 
 
-
-
-
-
-
-
-//document.querySelector("select[name=uf]").addEventListener("change", () => {
-//    console.log("mudei")
-//})
